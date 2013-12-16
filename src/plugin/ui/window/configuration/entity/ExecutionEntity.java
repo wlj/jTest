1ce6c7a6c5e4ab6d051f55ef5af7e79096f9a09c
wlj_2-008@outlook.com <wlj_2-008@outlook.com>
2013-12-16 21:41:58 +0800

    commit

---
+++ src/plugin/ui/window/configuration/entity/ExecutionEntity.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package plugin.ui.window.configuration.entity;
+
+/**
+ * 执行项
+ * @author wlj
+ *
+ */
+public class ExecutionEntity {
+	//严重等级
+	private SeverityLevelEntity serverityLevelEntity;
+	//选项
+	private Option4ExecutionEntity option4ExecutionEntity;
+	//搜索
+	
+	
+}
