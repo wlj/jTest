4749eae5484814344c541d489405ab4c1236bc8f
wlj <wlj_2-008@outlook.com>
2013-12-18 14:14:36 +0800

    目标

--- src/plugin/ui/window/configuration/entity/GoalEntity.java
+++ src/plugin/ui/window/configuration/entity/GoalEntity.java
@@ -1,7 +1,15 @@
 package plugin.ui.window.configuration.entity;
 
 public class GoalEntity {
+	/**
+	 * 静态
+	 */
 	private GoalItemEntity staticItem;
+	
 	private GoalItemEntity fixUnitTestProblems;
+	
+	private GoalItemEntity reviewUnverifiedUnitTestException;
+	
+	private GoalItemEntity reviewUnverifyOutcomes;
 	
 }
