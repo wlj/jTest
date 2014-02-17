2474ce390857785eef82a36eaa69a62749be18d0
wlj_2-008@outlook.com <wlj_2-008@outlook.com>
2014-02-17 20:49:02 +0800

    jjjjjjj

---
+++ src/plugin/ui/window/configuration/detailtabs/Static4GoalEntity.java
@@ -0,0 +1,7 @@
+package plugin.ui.window.configuration.detailtabs;
+
+import plugin.ui.window.configuration.entity.StaticGoalEntity;
+
+public class Static4GoalEntity {
+	public StaticGoalEntity staticGoalEntity;
+}
